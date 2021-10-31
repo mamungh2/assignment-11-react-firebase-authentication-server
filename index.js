29ef5d9ccaba771606965fc0093d5dde794e284c
Md. Rafiqul Islam Mamun <mamun8475@gmail.com>
2021-10-31 23:13:11 +0600

    readme added

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// use middleware
 app.use(cors());
 app.use(express.json());
 
-
+// connection string
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r7g7r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// create new instance of mongoclient class
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
@@ -68,11 +70,33 @@ async function run() {
         // delete api
         app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
-            console.log(id);
             const query = { _id: ObjectId(id) };
             const result = await ordersCollection.deleteOne(query);
-            console.log(result);
             res.json(result);
+        })
+
+        // get api for orders with single id
+        app.get('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const order = await ordersCollection.findOne(query);
+            res.send(order);
+        })
+
+        // update api
+        app.put('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedOrder = req.body;
+            console.log(updatedOrder);
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    status: updatedOrder.status
+                },
+            };
+            const result = await ordersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
         })
     }
     finally {
