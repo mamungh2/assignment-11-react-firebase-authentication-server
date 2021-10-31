fa985ef3cafe01c5d977d29b27d4fd2dd58f2f70
Md. Rafiqul Islam Mamun <mamun8475@gmail.com>
2021-10-31 19:02:12 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r7g7r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('connection successfull');
+        const database = client.db('mamunCourier');
+        const servicesCollection = database.collection('services');
+        const ordersCollection = database.collection('orders');
+
+        // post api for services
+        app.post('/services', async (req, res) => {
+            const newService = req.body;
+            const result = await servicesCollection.insertOne(newService);
+            res.json(result);
+        })
+
+        // get api for services
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        // get api for orders
+        app.get('/orders', async (req, res) => {
+            const cursor = ordersCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
+        // post api for orders
+        app.post('/orders', async (req, res) => {
+            const newOrder = req.body;
+            const result = await ordersCollection.insertOne(newOrder);
+            res.json(result);
+        })
+
+        // get api with dynamic serviceId
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.send(service);
+        })
+
+        // post api for orders to get data by name
+        app.post('/orders/byNames', async (req, res) => {
+            const loggedInUser = req.body;
+            const query = { name: { $in: loggedInUser } };
+            const orders = await ordersCollection.find(query).toArray();
+            res.json(orders);
+        })
+
+        // delete api
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            console.log(result);
+            res.json(result);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('This message from my server')
+});
+
+app.listen(port, () => {
+    console.log('Listening from port', port);
+})
